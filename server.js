7317c05426f909fd8c3fd9540aa40952d3597c89
hashmat1530 <hashmat2020vskp@gmail.com>
2024-06-18 17:58:23 +0530

    basic landing page

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express()
+
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
+
+
+app.listen(8000,()=>{
+    console.log("server is running on port 8000")
+});
